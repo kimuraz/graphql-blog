2a11d84476c5b29cb2200676e27cf588abece332
Kimura <joseduardo.kimura@gmail.com>
2019-01-10 20:43:14 -0200

    :bulb: Add Post schema

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
 const {GraphQLServer} = require('graphql-yoga');
 
-const typeDefs = `
-  type Query {
-    description: String
-  }
-`;
+let postId = 1;
+const posts = [];
+
 
 const resolvers = {
   Query: {
     description: () => 'Blog API',
+    posts: () => posts,
+    post: (parent, args) => posts.find(p => p.id === args.id),
+  },
+  Mutation: {
+    createDraft: (parent, args) => {
+      const post = {
+        id: `post_${postId}`,
+        title: args.title,
+        content: args.content,
+        published: false,
+      };
+      postId += 1;
+
+      posts.push(post);
+      return post;
+    },
+    deletePost: (parent, args) => {
+      const idx = posts.findIndex(p => p.id === args.id);
+      if (idx > -1) {
+        posts.splice(idx, 1);
+      }
+      return null;
+    },
+    publish: (parent, args) => {
+      const idx = posts.findIndex(p => p.id === args.id);
+      posts[idx].published = true;
+      return posts[idx];
+    },
   },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 
